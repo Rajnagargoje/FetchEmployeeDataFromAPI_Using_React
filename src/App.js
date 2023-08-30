b151f83f1b294029795340691b4dc40cc99d6063
Ganesh Nagargoje <ganesh.nagargoje@peopletech.com>
2023-08-30 12:49:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState(1);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+      );
+      const res = await get.json();
+      setData(res);
+    }
+    getData();
+    document.title = `(${state}) employee online`;
+  }, [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "30px" }}>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => setState(state + 1)}>Click Me {state}</button>
+      </div>
+
+      <table
+        style={{
+          background: "gray",
+          margin: "30px",
+          justifyContent: "space-between",
+        }}
+      >
+        <tr>
+          <th>Id</th>
+          <th>FirstName</th>
+          <th>LastName</th>
+          <th>Email</th>
+          <th>Conatct Number</th>
+          <th>Age</th>
+          <th>DOB</th>
+          <th>Salary</th>
+          <th>Address</th>
+        </tr>
+        {data.map((element, index) => {
+          return (
+            <tr key={index}>
+              <td>{element.id}</td>
+              <td>{element.firstName}</td>
+              <td>{element.lastName}</td>
+              <td>{element.email}</td>
+              <td>{element.contactNumber}</td>
+              <td>{element.age}</td>
+              <td>{element.dob}</td>
+              <td>{element.salary}</td>
+              <td>{element.address}</td>{" "}
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
-}
+};
 
 export default App;
